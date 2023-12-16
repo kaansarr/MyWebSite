fd4f4f5c94836f08601ffc3f760e6dff66a623b5
kaansarr <kaansar33@gmail.com>
2023-12-17 01:38:14 +0300

    update

--- MyWebSite/MyWebSite.WebUI/Controllers/HomeController.cs
+++ MyWebSite/MyWebSite.WebUI/Controllers/HomeController.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyWebSite.BL.Repositories;
+using MyWebSite.DAL.Entities;
+using MyWebSite.WebUI.ViewModel;
 
 namespace MyWebSite.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Hakkimda> repoHakkimda;
+
+        public HomeController(IRepository<Hakkimda> _repoHakkimda)
+        {
+            repoHakkimda= _repoHakkimda;
+        }
         public IActionResult Index()
         {
-            return View();
+            IndexVM indexVM = new IndexVM
+            {
+                Hakkimda = repoHakkimda.GetAll()
+            };
+            return View(indexVM);
         }
     }
 }
---
+++ MyWebSite/MyWebSite.WebUI/ViewModel/IndexVM.cs
@@ -0,0 +1,9 @@
+﻿using MyWebSite.DAL.Entities;
+
+namespace MyWebSite.WebUI.ViewModel
+{
+	public class IndexVM
+	{
+		public IEnumerable  <Hakkimda> Hakkimda { get; set; }
+	}
+}
