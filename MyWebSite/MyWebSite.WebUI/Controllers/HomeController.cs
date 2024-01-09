74e581871653d68b75b761724ba7dc41f734a2b6
kaansarr <kaansar33@gmail.com>
2024-01-09 23:33:36 +0300

    Proje Alt Fotoğrafları

--- MyWebSite/MyWebSite.WebUI/Controllers/HomeController.cs
+++ MyWebSite/MyWebSite.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MyWebSite.BL.Repositories;
+using MyWebSite.DAL.Contexts;
 using MyWebSite.DAL.Entities;
 using MyWebSite.WebUI.ViewModel;
 
@@ -8,64 +10,68 @@ using System.Linq;
 
 namespace MyWebSite.WebUI.Controllers
 {
-    public class HomeController : Controller
-    {
-        IRepository<Hakkimda> repoHakkimda;
-        IRepository<Yeteneklerim> repoYeteneklerim;
-        IRepository<Egitim> repoEgitimlerim;
-        IRepository<Iletisim> repoIletisim;
-        IRepository<Proje> repoProje;
-        IRepository<ProjePicture> repoProjePicture;
+	public class HomeController : Controller
+	{
+		IRepository<Hakkimda> repoHakkimda;
+		IRepository<Yeteneklerim> repoYeteneklerim;
+		IRepository<Egitim> repoEgitimlerim;
+		IRepository<Iletisim> repoIletisim;
+		IRepository<Proje> repoProje;
+		IRepository<ProjePicture> repoProjePicture;
+		SqlContext db;
 
-        
-        public HomeController( IRepository<Yeteneklerim> _repoYeteneklerim,IRepository<Hakkimda> _repoHakkimda, IRepository<Egitim> _repoEgitimlerim, IRepository<Iletisim> _repoIletisim, IRepository<Proje> _repoProje, IRepository<ProjePicture> _repoProjePicture)
-        {
-            repoEgitimlerim = _repoEgitimlerim;
-            repoYeteneklerim = _repoYeteneklerim;
-            repoHakkimda= _repoHakkimda;
-            repoIletisim= _repoIletisim;
-            repoProje = _repoProje;
-            repoProjePicture= _repoProjePicture;
-        }
-        public IActionResult Index()
-        {
-            IndexVM indexVM = new IndexVM
-            {
-                Egitimlerim = repoEgitimlerim.GetAll().OrderBy(o => o.ID).ToList()
+
+		public HomeController(IRepository<Yeteneklerim> _repoYeteneklerim, IRepository<Hakkimda> _repoHakkimda, IRepository<Egitim> _repoEgitimlerim, IRepository<Iletisim> _repoIletisim, IRepository<Proje> _repoProje, IRepository<ProjePicture> _repoProjePicture, SqlContext db)
+		{
+			repoEgitimlerim = _repoEgitimlerim;
+			repoYeteneklerim = _repoYeteneklerim;
+			repoHakkimda = _repoHakkimda;
+			repoIletisim = _repoIletisim;
+			repoProje = _repoProje;
+			repoProjePicture = _repoProjePicture;
+			this.db = db;
+		}
+		public IActionResult Index()
+		{
+			IndexVM indexVM = new IndexVM
+			{
+				Egitimlerim = repoEgitimlerim.GetAll().OrderBy(o => o.ID).ToList()
 ,
-                Hakkimda = repoHakkimda.GetAll(),
-                Yeteneklerim = repoYeteneklerim.GetAll(),
-                Projelerim = repoProje.GetAll()
-                
-                
-            };
-            return View(indexVM);
-        }
-        [HttpGet]
-        public PartialViewResult Iletisim()
-        {
-            return PartialView();
-        }
+				Hakkimda = repoHakkimda.GetAll(),
+				Yeteneklerim = repoYeteneklerim.GetAll(),
+				//Projelerim = repoProje.GetAll()
 
-		
+
+			};
+			indexVM.Projelerim = db.Projelerim.Include(x=>x.ProjePictures).ToList();
+
+			return View(indexVM);
+		}
+		[HttpGet]
+		public PartialViewResult Iletisim()
+		{
+			return PartialView();
+		}
+
+
 		[HttpPost]
-		public async Task <IActionResult> Iletisim(Iletisim t)
+		public async Task<IActionResult> Iletisim(Iletisim t)
 		{
 			await repoIletisim.Add(t);
 			return Redirect("/");
 		}
 
-		[Route("/Home/DetailGallery/{id}")]
-		public IActionResult DetailGallery(int id)
-		{
-			var viewModel = new ProjeDetailViewModel
-			{
-				Proje = repoProje.GetBy(o => o.ID == id),
-				ProjePictures = repoProjePicture.GetAll().Where(p => p.ProjeID == id).OrderByDescending(o => o.ID).ToList()
-			};
+		//[Route("/Home/DetailGallery/{id}")]
+		//public IActionResult DetailGallery(int id)
+		//{
+		//	var viewModel = new ProjeDetailViewModel
+		//	{
+		//		Proje = repoProje.GetBy(o => o.ID == id),
+		//		ProjePictures = repoProjePicture.GetAll().Where(p => p.ProjeID == id).OrderByDescending(o => o.ID).ToList()
+		//	};
 
-			return View(viewModel);
-		}
+		//	return View(viewModel);
+		//}
 
 
 
